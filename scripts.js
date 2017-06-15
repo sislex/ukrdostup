bef424b074767208e4a52d89a439b4511cfd07ff
sislex <sislex@ya.ru>
2017-06-15 07:36:37 +0300

    start

--- scripts.js
+++ scripts.js
@@ -6,18 +6,20 @@ $(document).ready(() => {
 function event() {
     $('button').bind('click', function () {
         var url = $(this).attr('url');
+        $("a.target").attr('href', url);
+        $("a.target").click();
         getLink(url);
         console.log(url);
     });
 }
 
 function getLink(url) {
-    $.get('/Api', {'url': url}, (response) => {
+    $.get('/Api.php', {'url': url}, (response) => {
         var params = "width=1,height=1,resizable=no,scrollbars=no,status=no";
-        var wind = window.open("https://ad.admitad.com/g/5351d3d56efbfe2254f5e6dcee139a/", "target", params);
+        // var wind = window.open("https://ad.admitad.com/g/5351d3d56efbfe2254f5e6dcee139a/", "target", params);
         setTimeout(function () {
-            wind.close();
-            location.href = response.result;
+            // wind.close();
+            // location.href = response.result;
         }, 3000 );
     }, 'json');
 }
